0ff27f2eb347906eabe037a942a675694ff983e1
yue <yueweichao0927@gmail.com>
2020-03-24 00:47:49 +0800

    文件读写第二版

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	_ "fmt"
+	"io"
+	"io/ioutil"
 	_ "lang/calc"
 	"os"
 )
@@ -11,10 +14,47 @@ const (
 	len int = 124
 )
 
-func main() {
-
-	//fmt.Println(calc.Jc(3))
+func fileTobufio() {
 	fh, err := os.Open("./calc/calc.go")
+	if err != nil {
+		fmt.Println("file open failed")
+		return
+	}
+	defer fh.Close()
+	reader := bufio.NewReader(fh)
+	for {
+		str, err := reader.ReadString('\n')
+
+		if err == io.EOF {
+			fmt.Println("读取到末尾")
+			return
+		}
+
+		if err != nil {
+			fmt.Println("read failed")
+			return
+		}
+
+		fmt.Println(str)
+	}
+
+}
+
+func readByIoUtil(str string) {
+	tmp, err := ioutil.ReadFile(str)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(tmp))
+
+}
+
+func main() {
+	//fileTobufio()
+	readByIoUtil("./calc/calc.go")
+	//fmt.Println(calc.Jc(3))
+	/*fh, err := os.Open("./calc/calc.go")
 	if err != nil {
 		fmt.Println("open file failed")
 		return
@@ -33,6 +73,6 @@ func main() {
 		if n < 128 {
 			return
 		}
-	}
+	}*/
 
 }
