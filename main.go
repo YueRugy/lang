d29c0a33ff3aafedc6d545ba12a32a59003d216b
yue <yueweichao0927@gmail.com>
2020-03-24 00:22:12 +0800

    文件读写第一版

--- main.go
+++ main.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
-	"lang/calc"
+	_ "fmt"
+	_ "lang/calc"
+	"os"
+)
+
+const (
+	len int = 124
 )
 
 func main() {
 
-	fmt.Println(calc.Jc(3))
+	//fmt.Println(calc.Jc(3))
+	fh, err := os.Open("./calc/calc.go")
+	if err != nil {
+		fmt.Println("open file failed")
+		return
+	}
+	defer fh.Close()
+
+	var buffer [128]byte
+	for {
+		n, err := fh.Read(buffer[:])
+		if err != nil {
+			fmt.Println("file read failed")
+			return
+		}
+		fmt.Printf("读了%d个字节", n)
+		fmt.Println(string(buffer[:n]))
+		if n < 128 {
+			return
+		}
+	}
+
 }
