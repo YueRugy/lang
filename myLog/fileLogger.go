b157e3455ff7ce43fccf362e45237b7fed76c504
yue <yueweichao0927@gmail.com>
2020-03-25 07:20:47 +0800

    日志分割,注意下指针

--- myLog/fileLogger.go
+++ myLog/fileLogger.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"time"
 )
 
 type fileLogger struct {
@@ -43,12 +44,48 @@ func NewFileLogger(fp, fn, level string, ms uint64) *fileLogger {
 		errFh:    fhErr,
 	}
 }
+func (fl *fileLogger) checkSize(file *os.File) bool {
+	info, err := file.Stat()
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	return uint64(info.Size()) >= fl.maxSize
+}
 
-func fileLog(fl fileLogger, level logLevel, format string, a ...interface{}) {
+func (fl *fileLogger) spiltFile(file *os.File) (*os.File, error) {
+
+	var newFile *os.File
+
+	str := time.Now().Format("2006010215040500")
+	fName := file.Name()
+	file.Close()
+	err := os.Rename(fName, fName+str)
+	/*if err != nil {
+		fmt.Printf("rename file failed %v", err)
+		return file, err
+	}*/
+	file.Close()
+
+	newFile, err = os.OpenFile(fName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("open new file failed %v", err)
+		return nil, err
+	}
+	return newFile, nil
+}
+func fileLog(fl *fileLogger, level logLevel, format string, a ...interface{}) {
 	if judge(fl.level, level) {
 		sli := info(3)
 		msg := fmt.Sprintf(format, a...)
 		line, _ := strconv.Atoi(sli[2])
+		if fl.checkSize(fl.fh) {
+			newFile, err := fl.spiltFile(fl.fh)
+			if err != nil {
+				fmt.Printf("%v", err)
+				return
+			}
+			fl.fh = newFile
+		}
 		_, err := fmt.Fprint(fl.fh, fmt.Sprintf("[%s] [%s] [%s:%s:%d] [%s]\n", timeParse(),
 			unParse(level), sli[1], sli[0], line, msg))
 		if err != nil {
@@ -57,10 +94,16 @@ func fileLog(fl fileLogger, level logLevel, format string, a ...interface{}) {
 		}
 
 		if level >= ERROR {
-
-			_, err := fmt.Fprint(fl.errFh, fmt.Sprintf("[%s] [%s] [%s:%s:%d] [%s]\n", timeParse(),
+			if fl.checkSize(fl.errFh) {
+				newFile, err := fl.spiltFile(fl.errFh)
+				if err != nil {
+					fmt.Printf("%v", err)
+					return
+				}
+				fl.errFh = newFile
+			}
+			_, err = fmt.Fprint(fl.errFh, fmt.Sprintf("[%s] [%s] [%s:%s:%d] [%s]\n", timeParse(),
 				unParse(level), sli[1], sli[0], line, msg))
-
 			if err != nil {
 				fmt.Printf("write errlog to file failed%v", err)
 				return
@@ -70,29 +113,29 @@ func fileLog(fl fileLogger, level logLevel, format string, a ...interface{}) {
 	}
 }
 
-func (fl fileLogger) Debug(format string, a ...interface{}) {
+func (fl *fileLogger) Debug(format string, a ...interface{}) {
 	fileLog(fl, DEBUG, format, a)
 }
 
-func (fl fileLogger) Trace(format string, a ...interface{}) {
+func (fl *fileLogger) Trace(format string, a ...interface{}) {
 	fileLog(fl, TRACE, format, a)
 }
 
-func (fl fileLogger) Info(format string, a ...interface{}) {
+func (fl *fileLogger) Info(format string, a ...interface{}) {
 	fileLog(fl, INFO, format, a)
 }
-func (fl fileLogger) Warning(format string, a ...interface{}) {
+func (fl *fileLogger) Warning(format string, a ...interface{}) {
 	fileLog(fl, WARRING, format, a)
 }
 
-func (fl fileLogger) Error(format string, a ...interface{}) {
+func (fl *fileLogger) Error(format string, a ...interface{}) {
 	fileLog(fl, ERROR, format, a)
 }
-func (fl fileLogger) Fatal(format string, a ...interface{}) {
+func (fl *fileLogger) Fatal(format string, a ...interface{}) {
 	fileLog(fl, FATAL, format, a)
 }
 
-func (fl fileLogger) FClose() {
+func (fl *fileLogger) FClose() {
 	fl.errFh.Close()
 	fl.fh.Close()
 }
