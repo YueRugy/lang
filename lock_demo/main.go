92919935e1406c424ece85da2b32bbcb94a83cb6
yue <yueweichao0927@gmail.com>
2020-03-26 10:32:04 +0800

    read-write lock test

--- lock_demo/main.go
+++ lock_demo/main.go
@@ -3,29 +3,58 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 var (
 	x    int
 	wait sync.WaitGroup
 	lock sync.Mutex
+
+	rwLock sync.RWMutex
 )
 
-func add() {
+func write() {
 	defer wait.Done()
 	//defer lock.Unlock()
-	for i := 0; i < 5000; i++ {
-		lock.Lock()
+	for i := 0; i < 50000; i++ {
+		rwLock.Lock()
+		//	lock.Lock()
 		x += 1
-		lock.Unlock()
+		//	lock.Unlock()
+		rwLock.Unlock()
+
 	}
 }
 
+func read() {
+	defer rwLock.RUnlock()
+	//	defer lock.Unlock()
+	defer wait.Done()
+	rwLock.RLock()
+	//lock.Lock()
+	fmt.Println(x)
+
+}
+
 func main() {
-	wait.Add(2)
-	go add()
-	go add()
+
+	currentTime := time.Now()
+	for i := 0; i < 2; i++ {
+		wait.Add(1)
+		go write()
+	}
+
+	for i := 0; i < 1000; i++ {
+		wait.Add(1)
+		go read()
+	}
+
+	//wait.Add(2)
+	//go add()
+	//go add()
 	wait.Wait()
 
-	fmt.Println(x)
+	fmt.Println(time.Now().Sub(currentTime))
+	//fmt.Println(x)
 }
