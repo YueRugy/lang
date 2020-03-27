5ef97de7a51a454ad6f5c0dab295fb50db91d1f5
yue <yueweichao0927@gmail.com>
2020-03-27 10:07:44 +0800

    增删改查基本版

--- mysql_demo2/main.go
+++ mysql_demo2/main.go
@@ -19,33 +19,48 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+	deleteById(1)
+	//insert()
+	//updateDemo()
+	//query1 := "select id,name,age from user where id =?"
+	//var u user
+	//err1 := queryById(query1, 1, &u)
+	//if err1 != nil {
+	//	fmt.Println(u)
+	//}
+	//query2 := "select id,name,age from user where id>?"
+	//rows, err2 := query(query2, 0)
+	//if err2 != nil {
+	//	fmt.Println("查询失败")
+	//	return
+	//}
 
-	query1 := "select id,name,age from user where id =?"
-	var u user
-	err1 := queryById(query1, 1, &u)
-	if err1 != nil {
-		fmt.Println(u)
-	}
-	query2 := "select id,name,age from user where id>?"
-	rows, err2 := query(query2, 0)
-	if err2 != nil {
-		fmt.Println("查询失败")
-		return
-	}
+	//defer rows.Close()
+	//for ; rows.Next(); {
+	//	var u1 user
+	//	err = rows.Scan(&u1.id, &u1.name, &u1.age)
+	//	if err != nil {
+	//		return
+	//	}
 
-	defer rows.Close()
-	for ; rows.Next(); {
-		var u1 user
-		err = rows.Scan(&u1.id, &u1.name, &u1.age)
-		if err != nil {
-			return
-		}
-
-		fmt.Println(u1)
-	}
+	//	fmt.Println(u1)
+	//}
 
 }
 
+func updateDemo() {
+	dsn := "update user set name=? where id = ?"
+	ret, err := db.Exec(dsn, "hha", 1)
+	if err != nil {
+		fmt.Println("update failed ")
+		return
+	}
+	n, err2 := ret.RowsAffected()
+	if err2 != nil {
+		fmt.Println("get affected failed")
+	}
+	fmt.Println(n)
+}
 func query(query string, a ...interface{}) (rows *sql.Rows, err error) {
 	rows, err = db.Query(query, a...)
 	if err != nil {
@@ -62,6 +77,36 @@ func queryById(query string, id int, u *user) (err error) {
 		return
 	}
 	return nil
+}
+
+func deleteById(id int) {
+	strSql := "delete from user where id =?"
+	ret, err := db.Exec(strSql, id)
+	if err != nil {
+		fmt.Println("delete failed", err)
+		return
+	}
+	n, err1 := ret.RowsAffected()
+	if err1 != nil {
+		fmt.Println("get affected failed", err1)
+	}
+	fmt.Println(n)
+}
+func insert() {
+	dsn := `insert into user (name,age) values ("ge",29)`
+	ret, err := db.Exec(dsn)
+	if err != nil {
+		fmt.Println("insert failed", err)
+		return
+	}
+
+	id, err1 := ret.LastInsertId()
+	if err1 != nil {
+		fmt.Println("get id failed", err1)
+	}
+
+	fmt.Println(id)
+
 }
 
 func initDB() (err error) {
